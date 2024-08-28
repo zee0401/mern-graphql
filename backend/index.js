e3aa62eee625b54821faccbf5e83d85527fc5fcb
zee0401 <41227625+zee0401@users.noreply.github.com>
2024-08-28 14:09:18 +0530

    passport js configuration

--- backend/index.js
+++ backend/index.js
@@ -7,15 +7,48 @@ import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
 import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
 
+import passport from "passport";
+import session from "express-session";
+import connectMongo from "connect-mongodb-session";
+
 import mergedTypeDefs from "./typeDefs/index.js";
 import mergedResolvers from "./resolvers/index.js";
 
 import { connectDB } from "./database/connectDB.js";
+import { buildContext } from "graphql-passport";
+
+import { configurePassport } from "./passport/passport.config.js";
 
 const app = express();
 dotenv.config();
+configurePassport();
 
 const httpServer = http.createServer(app);
+
+const MongoDBStore = connectMongo(session);
+
+const store = new MongoDBStore({
+  uri: process.env.MONGODB_URI,
+  Collection: "sessions",
+});
+
+store.on("error", (err) => {
+  console.log(err);
+});
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 * 7 },
+    httpOnly: true,
+    store: store,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 const server = new ApolloServer({
   typeDefs: mergedTypeDefs,
@@ -27,10 +60,13 @@ await server.start();
 
 app.use(
   "/",
-  cors(),
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  }),
   express.json(),
   expressMiddleware(server, {
-    context: async ({ req }) => ({ req }),
+    context: async ({ req, res }) => buildContext({ req }),
   })
 );
 
---
+++ backend/passport/passport.config.js
@@ -0,0 +1,42 @@
+import passport from "passport";
+import { GraphQLLocalStrategy } from "graphql-passport";
+import bcrypt from "bcryptjs";
+
+import User from "../models/user.model.js";
+
+export const configurePassport = async () => {
+  passport.serializeUser((user, done) => {
+    console.log("serializeUser", user);
+    done(null, user.id);
+  });
+
+  passport.deserializeUser(async (id, done) => {
+    console.log("deserializeUser");
+    try {
+      const user = await User.findById(id);
+      done(null, user);
+    } catch (error) {
+      done(error, null);
+      console.log(error);
+    }
+  });
+
+  passport.use(
+    new GraphQLLocalStrategy(User, async (username, password, done) => {
+      try {
+        const user = await User.findOne({ username });
+        if (!user) {
+          throw new Error("Invalid username or password");
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+          return done(null, false, { message: "Password incorrect" });
+        }
+        return done(null, user);
+      } catch (error) {
+        console.log(error);
+        return done(error);
+      }
+    })
+  );
+};
